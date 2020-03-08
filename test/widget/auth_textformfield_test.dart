62c053d62fb0367d2f328b22fc59e51bd81a77f9
Philippe Fanaro <philippefanaro@gmail.com>
2020-03-08 15:08:34 -0300

    separated the tests into groups

--- test/widget/auth_textformfield_test.dart
+++ test/widget/auth_textformfield_test.dart
@@ -4,31 +4,38 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_firebase_auth_benchmark/widgets/login/auth_textformfield.dart';
 
 void main() {
-  testWidgets(
-      'Checks that there is no visibility icon '
-      'if `obscureText` is `false`', (tester) async {
-    final AuthTextFormField authFieldDummy = AuthTextFormField(
-      key: Key('email_field'),
-      keyboardType: TextInputType.emailAddress,
-      hintText: 'email',
-      labelText: 'email',
-      icon: Icons.account_circle,
-    );
 
-    final MaterialApp wrappedWidget = MaterialApp(
-        home: Material(
-      child: authFieldDummy,
-    ));
+  group('Email Field Use Case', (){
+    testWidgets(
+        'Checks that there is no visibility icon '
+        'if `obscureText` is `false`', (tester) async {
+      final AuthTextFormField authFieldDummy = AuthTextFormField(
+        key: Key('email_field'),
+        keyboardType: TextInputType.emailAddress,
+        hintText: 'email',
+        labelText: 'email',
+        icon: Icons.account_circle,
+      );
 
-    await tester.pumpWidget(wrappedWidget);
+      final MaterialApp wrappedWidget = MaterialApp(
+          home: Material(
+        child: authFieldDummy,
+      ));
 
-    final StatefulElement extractedAuthFieldElement =
-        tester.element(find.byType(AuthTextFormField));
-    final AuthTextFormFieldState extractedAuthFieldState =
-        extractedAuthFieldElement.state as AuthTextFormFieldState;
+      await tester.pumpWidget(wrappedWidget);
 
-    expect(extractedAuthFieldState.widget, equals(authFieldDummy));
-    expect(extractedAuthFieldState.doNotObscureText, isTrue);
-    expect(find.byType(IconData), findsNothing);
+      final StatefulElement extractedAuthFieldElement =
+          tester.element(find.byType(AuthTextFormField));
+      final AuthTextFormFieldState extractedAuthFieldState =
+          extractedAuthFieldElement.state as AuthTextFormFieldState;
+
+      expect(extractedAuthFieldState.widget, equals(authFieldDummy));
+      expect(extractedAuthFieldState.doNotObscureText, isTrue);
+      expect(find.byType(IconData), findsNothing);
+    });
+  });
+  
+  group('Password Field Use Case', (){
+    
   });
 }
