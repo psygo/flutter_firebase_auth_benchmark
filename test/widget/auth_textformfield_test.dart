324e096a0b773a34ef6eaaa1545f54b3239e69de
Philippe Fanaro <philippefanaro@gmail.com>
2020-03-08 15:16:59 -0300

    refactored the auth_textformfield tests | starting the password field use case

--- test/widget/auth_textformfield_test.dart
+++ test/widget/auth_textformfield_test.dart
@@ -4,25 +4,26 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_firebase_auth_benchmark/widgets/login/auth_textformfield.dart';
 
 void main() {
+  MaterialApp materialWrapper(Widget widget) => MaterialApp(
+      home: Material(
+    child: widget,
+  ));
 
   group('Email Field Use Case', (){
+    final AuthTextFormField authFieldDummy = AuthTextFormField(
+      key: Key('email_field'),
+      keyboardType: TextInputType.emailAddress,
+      hintText: 'email',
+      labelText: 'email',
+      icon: Icons.account_circle,
+    );
+
+    final MaterialApp wrappedAuthField = materialWrapper(authFieldDummy);
+
     testWidgets(
         'Checks that there is no visibility icon '
         'if `obscureText` is `false`', (tester) async {
-      final AuthTextFormField authFieldDummy = AuthTextFormField(
-        key: Key('email_field'),
-        keyboardType: TextInputType.emailAddress,
-        hintText: 'email',
-        labelText: 'email',
-        icon: Icons.account_circle,
-      );
-
-      final MaterialApp wrappedWidget = MaterialApp(
-          home: Material(
-        child: authFieldDummy,
-      ));
-
-      await tester.pumpWidget(wrappedWidget);
+      await tester.pumpWidget(wrappedAuthField);
 
       final StatefulElement extractedAuthFieldElement =
           tester.element(find.byType(AuthTextFormField));
@@ -36,6 +37,21 @@ void main() {
   });
   
   group('Password Field Use Case', (){
-    
+    final AuthTextFormField authFieldDummy = AuthTextFormField(
+      key: Key('password_field'),
+      keyboardType: TextInputType.visiblePassword,
+      hintText: 'password',
+      labelText: 'password',
+      icon: Icons.lock,
+      obscureText: true,
+    );
+
+    final MaterialApp wrappedAuthField = materialWrapper(authFieldDummy);
+
+    testWidgets(
+      'Checks that there is a visibility icon available '
+      'when `obscureText` is `true`', (tester) async {
+        
+    });
   });
 }
