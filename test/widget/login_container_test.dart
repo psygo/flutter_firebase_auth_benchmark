507743cf379aa231d0ba3be6b4d8427241c92f89
Philippe Fanaro <philippefanaro@gmail.com>
2020-03-12 11:09:10 -0300

    tests are passing again

--- test/widget/login_container_test.dart
+++ test/widget/login_container_test.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
 
+import 'package:flutter_firebase_auth_benchmark/providers/login_workflow_provider.dart';
 import 'package:flutter_firebase_auth_benchmark/widgets/login/login_container.dart';
 
 import 'utils.dart';
 
 void main() {
-  final LoginContainer loginContainer = LoginContainer(child: Container());
+  final LoginContainer loginContainer = LoginContainer(
+    child: Consumer<LoginWorkflowHandler>(
+      builder: (context, loginWorkflowProvider, _){
+        return AnimatedContainer(duration: Duration(milliseconds: 500));
+      }
+    ),
+  );
+  final wrappedLoginContainer = WidgetWrappers.materialWrapperWithLoginWorkflowProvider(loginContainer);
 
   testWidgets('Tests LoginContainer Initialization', (tester) async {
-    await tester.pumpWidget(loginContainer);
+    await tester.pumpWidget(wrappedLoginContainer);
 
     final LoginContainer extractedLoginContainer =
         WidgetExtractor.extractStatelessWidgetByType(tester, LoginContainer);
@@ -19,6 +28,6 @@ void main() {
 
   testWidgets('Tests that the LoginContainer has a child property',
       (tester) async {
-    expect(loginContainer.child, isInstanceOf<Container>());
+    expect(loginContainer.child, isInstanceOf<Consumer>());
   });
 }
