3518edbdd00e6ade66344a5eb8394173f20336bd
Philippe Fanaro <philippefanaro@gmail.com>
2020-03-13 09:22:03 -0300

    dartfmt

--- lib/authentication/validator.dart
+++ lib/authentication/validator.dart
@@ -1,22 +1,22 @@
 abstract class AuthenticationValidator {
   static String emptyEmailMsg = 'Please type your email.';
-  static String confirmPasswordDoesNotMatchMsg = 
-    'Your password and your confirmation password don\'t match.';
-  static String passwordTooShortMsg = 
-    'Your password must be longer than 6 characters.';
+  static String confirmPasswordDoesNotMatchMsg =
+      'Your password and your confirmation password don\'t match.';
+  static String passwordTooShortMsg =
+      'Your password must be longer than 6 characters.';
 
-  static String validateEmail(String email) 
-    => email.isNotEmpty ? null : emptyEmailMsg;
+  static String validateEmail(String email) =>
+      email.isNotEmpty ? null : emptyEmailMsg;
 
-  static String validatePassword(String password){
-    if (password.length < 6){
+  static String validatePassword(String password) {
+    if (password.length < 6) {
       return passwordTooShortMsg;
-    }
-    else {
+    } else {
       return null;
     }
   }
 
-  static String validateConfirmPassword(String password, String confirmPassword)
-    => password == confirmPassword ? null : confirmPasswordDoesNotMatchMsg;
+  static String validateConfirmPassword(
+          String password, String confirmPassword) =>
+      password == confirmPassword ? null : confirmPasswordDoesNotMatchMsg;
 }
--- test/unit/validator_test.dart
+++ test/unit/validator_test.dart
@@ -2,35 +2,37 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_firebase_auth_benchmark/authentication/validator.dart';
 
-void main(){
-  group('Email Validation', (){
-    test('Can email validation handle an empty field?', (){
-      final String emailValidationResult = AuthenticationValidator
-        .validateEmail('');
+void main() {
+  group('Email Validation', () {
+    test('Can email validation handle an empty field?', () {
+      final String emailValidationResult =
+          AuthenticationValidator.validateEmail('');
 
       expect(emailValidationResult, AuthenticationValidator.emptyEmailMsg);
     });
 
-    test('', (){
+    test('', () {});
+  });
 
+  group('Password Validation', () {
+    test('Password length is smaller than 6', () {
+      final String passwordValidationResult =
+          AuthenticationValidator.validatePassword('asdf');
+
+      expect(passwordValidationResult,
+          AuthenticationValidator.passwordTooShortMsg);
     });
   });
 
-  group('Password Validation', (){
-    test('Password length is smaller than 6', (){
-      final String passwordValidationResult = AuthenticationValidator
-        .validatePassword('asdf');
+  group('Confirm Password Validation', () {
+    test('Results in error when password and confirm password are different',
+        () {
+      final String confirmPasswordValidationResult =
+          AuthenticationValidator.validateConfirmPassword(
+              'asdfasdf', 'asdfasdfg');
 
-      expect(passwordValidationResult, AuthenticationValidator.passwordTooShortMsg);
-    });
-  });
-
-  group('Confirm Password Validation', (){
-    test('Results in error when password and confirm password are different', (){
-      final String confirmPasswordValidationResult = AuthenticationValidator
-        .validateConfirmPassword('asdfasdf', 'asdfasdfg');
-
-      expect(confirmPasswordValidationResult, AuthenticationValidator.confirmPasswordDoesNotMatchMsg);
+      expect(confirmPasswordValidationResult,
+          AuthenticationValidator.confirmPasswordDoesNotMatchMsg);
     });
   });
 }
