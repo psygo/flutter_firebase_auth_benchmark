1011530f24e47cc34dc00d950e64d6b48a889be6
Philippe Fanaro <philippefanaro@gmail.com>
2020-03-13 10:43:18 -0300

    email validation is now complete

--- lib/authentication/validator.dart
+++ lib/authentication/validator.dart
@@ -35,7 +35,7 @@ abstract class AuthenticationMsgs {
 
 abstract class AuthenticationUtils {
   static final RegExp emailRegExp = RegExp(
-      r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
 
   static bool passwordIsTooShort(String password) => password.length < 6;
 }
--- test/unit/validator_test.dart
+++ test/unit/validator_test.dart
@@ -5,9 +5,13 @@ import 'package:flutter_firebase_auth_benchmark/authentication/validator.dart';
 void main() {
   group('Email Validation', () {
     test('Tests different benchmarked emails', () {
-      Map<String, String> emailByCorrectValidationResultMsg = {
+      final Map<String, String> emailByCorrectValidationResultMsg = {
         '': AuthenticationMsgs.emptyEmail,
         'johndoe@gmail.com': null,
+        '.wooly@example.com': AuthenticationMsgs.invalidEmail,
+        'wo..oly@example.com': AuthenticationMsgs.invalidEmail,
+        'a@p.com': null,
+        'invalid:email@examp': AuthenticationMsgs.invalidEmail,
       };
 
       emailByCorrectValidationResultMsg
