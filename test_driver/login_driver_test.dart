a175317de346398c9b6b311168a8b7adb64c908b
Philippe Fanaro <philippefanaro@gmail.com>
2020-03-21 14:28:13 -0300

    sign in integration tests are now working | more refactoring

--- test_driver/login_driver_test.dart
+++ test_driver/login_driver_test.dart
@@ -1,8 +1,10 @@
-import 'package:meta/meta.dart';
 import 'package:flutter_driver/flutter_driver.dart';
+import 'package:meta/meta.dart';
 import 'package:test/test.dart';
 
 void main() {
+  const Duration firebaseStandardTimeout = Duration(seconds: 5);
+
   FlutterDriver driver;
 
   setUpAll(() async {
@@ -36,22 +38,22 @@ void main() {
     }
 
     test('Successful Sign Up', () async {
-      const String dummyEmail = 'ppsf@gmail.com';
+      const String dummySignUpEmail = 'ppsf@gmail.com';
       const String dummyPassword = 'asdfA\$D1';
 
-      await signUp(email: dummyEmail, password: dummyPassword);
+      await signUp(email: dummySignUpEmail, password: dummyPassword);
 
-      expect(
-          await driver.getText(find.byValueKey('user_email_text')), dummyEmail);
+      expect(await driver.getText(find.byValueKey('user_email_text')),
+          dummySignUpEmail);
 
       await driver.tap(find.byValueKey('delete_account_button'));
     });
 
     test('Email already in use', () async {
-      const String dummyEmail = 'pf@gmail.com';
+      const String permanentEmail = 'pf@gmail.com';
       const String dummyPassword = 'asdfA\$D1';
 
-      await signUp(email: dummyEmail, password: dummyPassword);
+      await signUp(email: permanentEmail, password: dummyPassword);
 
       expect(await driver.getText(find.text('user already exists')),
           'user already exists');
@@ -59,22 +61,53 @@ void main() {
   });
 
   group('Sign In', () {
-    const String dummyPermanentEmail = 'pf@gmail.com';
-    const String dummyPassword = 'asdfA\$D1';
-
-    test('Successful Sign In', () async {
+    Future<void> signIn({
+      @required email,
+      @required password,
+    }) async {
       await driver.tap(find.byValueKey('email_field'));
-      await driver.enterText(dummyPermanentEmail);
+      await driver.enterText(email);
 
       await driver.tap(find.byValueKey('password_field'));
-      await driver.enterText(dummyPassword);
+      await driver.enterText(password);
 
       await driver.tap(find.byValueKey('login_button'));
+    }
+
+    test('Successful Sign In', () async {
+      const String permanentEmail = 'pf@gmail.com';
+      const String dummyPassword = 'asdfA\$D1';
+
+      await signIn(email: permanentEmail, password: dummyPassword);
 
       expect(await driver.getText(find.byValueKey('logged_in_text')),
           'You\'re logged in!');
 
       await driver.tap(find.byValueKey('sign_out_button'));
+    });
+
+    test('User not found', () async {
+      const String fakeUserEmail = 'pf@gmail.co';
+      const String dummyPassword = 'asdfA\$D1';
+
+      await signIn(email: fakeUserEmail, password: dummyPassword);
+
+      expect(
+          await driver.getText(find.text('user not found'),
+              timeout: firebaseStandardTimeout),
+          'user not found');
+    });
+
+    test('Wrong password', () async {
+      const String permanentEmail = 'pf@gmail.com';
+      const String wrongPassword = 'asdfA\$D11';
+
+      await signIn(email: permanentEmail, password: wrongPassword);
+
+      expect(
+          await driver.getText(find.text('wrong password'),
+              timeout: firebaseStandardTimeout),
+          'wrong password');
     });
   });
 
@@ -85,7 +118,7 @@ void main() {
     tearDownAll(
         () async => await driver.tap(find.byValueKey('cancel_reset_button')));
 
-    void resetPassword({@required email}) async {
+    Future<void> resetPassword({@required email}) async {
       await driver.tap(find.byValueKey('email_field'));
       await driver.enterText(email);
 
@@ -93,23 +126,24 @@ void main() {
     }
 
     test('Simple password reset', () async {
-      const String dummyPermanentEmail = 'windbaduk@gmail.com';
+      const String permanentEmail = 'windbaduk@gmail.com';
 
-      resetPassword(email: dummyPermanentEmail);
+      await resetPassword(email: permanentEmail);
 
       expect(
-          await driver.getText(find.text('✓'), timeout: Duration(seconds: 3)),
+          await driver.getText(find.text('✓'),
+              timeout: firebaseStandardTimeout),
           '✓');
     });
 
     test('User not found error message', () async {
-      const String dummyUserNotFoundEmail = 'pf@gmail.co';
+      const String userNotFoundEmail = 'pf@gmail.co';
 
-      resetPassword(email: dummyUserNotFoundEmail);
+      await resetPassword(email: userNotFoundEmail);
 
       expect(
           await driver.getText(find.text('user not found'),
-              timeout: Duration(seconds: 3)),
+              timeout: firebaseStandardTimeout),
           'user not found');
     });
   });
