53a5444ba2e8d380ab5b2c2c9ede3d8e115968e2
Philippe Fanaro <philippefanaro@gmail.com>
2020-03-21 14:06:42 -0300

    refactored password reset integration tests even more

--- test_driver/login_driver_test.dart
+++ test_driver/login_driver_test.dart
@@ -85,13 +85,17 @@ void main() {
     tearDownAll(
         () async => await driver.tap(find.byValueKey('cancel_reset_button')));
 
+    void resetPassword({@required email}) async {
+      await driver.tap(find.byValueKey('email_field'));
+      await driver.enterText(email);
+
+      await driver.tap(find.byValueKey('send_password_reset_button'));
+    }
+
     test('Simple password reset', () async {
       const String dummyPermanentEmail = 'windbaduk@gmail.com';
 
-      await driver.tap(find.byValueKey('email_field'));
-      await driver.enterText(dummyPermanentEmail);
-
-      await driver.tap(find.byValueKey('send_password_reset_button'));
+      resetPassword(email: dummyPermanentEmail);
 
       expect(
           await driver.getText(find.text('✓'), timeout: Duration(seconds: 3)),
@@ -101,10 +105,7 @@ void main() {
     test('User not found error message', () async {
       const String dummyUserNotFoundEmail = 'pf@gmail.co';
 
-      await driver.tap(find.byValueKey('email_field'));
-      await driver.enterText(dummyUserNotFoundEmail);
-
-      await driver.tap(find.byValueKey('send_password_reset_button'));
+      resetPassword(email: dummyUserNotFoundEmail);
 
       expect(
           await driver.getText(find.text('user not found'),
